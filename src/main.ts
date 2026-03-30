382deba7eb121ef2c681625838ce68d031e8484b
IzharNabeh <mizahar@nabeh.sa>
2026-03-30 16:56:29 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log("This is just for testing");
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
